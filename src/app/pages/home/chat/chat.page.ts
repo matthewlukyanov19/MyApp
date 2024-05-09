b54a2f3db5e27a4983517c55b62c76070269af6c
matthewlukyanov19 <brucedog9@gmail.com>
2024-05-10 00:29:06 +0100

    I customized the chat-box components to allow texting and the design of it when sending a message.

--- src/app/pages/home/chat/chat.page.ts
+++ src/app/pages/home/chat/chat.page.ts
@@ -11,7 +11,7 @@ export class ChatPage implements OnInit {
   message: string;
   isLoading = false;
   currentUserId = 1;
-  @Input() chat: any;
+  //chat: any;
   chats = [
     {id: 1, sender: 1, message: 'hi'},
     {id: 2, sender: 2, message: 'bye'},
@@ -23,7 +23,20 @@ export class ChatPage implements OnInit {
   }
 
   sendMessage() {
+     // Add the user's message to the chat
+    this.chats.push({ id: this.chats.length + 1, sender: this.currentUserId, message: this.message });
+    
+    // Generate a random response
+    const responses = ["Hello!", "How are you?", "Nice to hear from you!", "What's up?", "I'm here."];
+    const randomIndex = Math.floor(Math.random() * responses.length);
+    const randomResponse = responses[randomIndex];
 
-  }
+    // Add the random response to the chat
+    this.chats.push({ id: this.chats.length + 1, sender: 2, message: randomResponse });
+    
+    // Clear the message input
+    this.message = '';
 
 }
+}
+
